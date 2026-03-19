9b6418ede3dc9eda81bdd4a7fa28bf150c22e3a0
Jarrod Flesch <jarrodmflesch@gmail.com>
2026-03-19 10:12:35 -0400

    chore rm cloud plugin and lock

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -1,6 +1,5 @@
 // storage-adapter-import-placeholder
 import { postgresAdapter } from '@payloadcms/db-postgres'
-import { payloadCloudPlugin } from '@payloadcms/payload-cloud'
 import { mcpPlugin } from '@payloadcms/plugin-mcp'
 import { lexicalEditor } from '@payloadcms/richtext-lexical'
 import path from 'path'
@@ -35,7 +34,6 @@ export default buildConfig({
   }),
   sharp,
   plugins: [
-    payloadCloudPlugin(),
     mcpPlugin({
       collections: {
         posts: {
