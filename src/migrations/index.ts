1415867aaeab3b987c15594073e1a9f9a8ed526b
Jarrod Flesch <jarrodmflesch@gmail.com>
2026-03-19 10:10:04 -0400

    chore: add initial migration

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20260319_140844 from './20260319_140844';
+
+export const migrations = [
+  {
+    up: migration_20260319_140844.up,
+    down: migration_20260319_140844.down,
+    name: '20260319_140844'
+  },
+];
